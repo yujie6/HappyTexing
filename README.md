7763dac9c584089054aa506632c062b13f2d9b75
yujie6 <yujie6@sjtu.edu.cn>
2019-10-12 23:38:07 +0800

    update dl script

--- README.md
+++ README.md
@@ -5,15 +5,14 @@
 
 
 <img src="./figures/texenv.png" alt="">
-
-所有的配置都只适用于 linux (我用的Ubuntu 19.04)，也许在macos下面也能用. 希望他们对你有所帮助
-## 序言 
-如果你比较懒的话，我写了一个脚本来实现自动安装和配置。不过最好还是看一看配置的原理方便日后使用。
+用LaTeX写作业的时候，总是苦恼速度太慢，于是有了这个仓库。
+所有的配置都只适用于 linux (我用的Ubuntu 19.04)，也许在macos下面也能用.
+ 
+你可以使用脚本安装所有工具或者自己下载。
 ```bash
 git clone https://github.com/yujie6/HappyTexing.git
 sudo bash install.sh
 ```
-
 ## 1. 加速工具
 
 ### 1.1 Vimtex
@@ -27,24 +26,23 @@ Vimtex 集成了 `latexmk`, 一个可以实时编译的tex工具（texlive自带
 
 ### 1.2 UltiSnips
 
-这是提速的核心. 我强烈推荐安装这个vim插件来进行latex的编辑, 你可以查看 <a href="https://castel.dev/post/lecture-notes-1/">这个博客</a> 窥见他有多么的方便。
+这是提速的核心. 我强烈推荐安装这个vim插件来加速latex的编辑. 
+你可以查看 <a href="https://castel.dev/post/lecture-notes-1/">这个博客</a> 窥见他有多么的方便。
 
 ### 1.3 preamble.tex
 这是一个保存自定义命令和宏的头文件.
-每次我打开一个新的tex文件时，我只需添加一行便可以开始我的编辑了
 
     \input{../preamble.tex}
 
-如果你在编辑中文, 最好使用 `\documentclass{ctexart}`. 并且你应该将 `xelatex`作为你的编译器。
+编辑中文最好使用 `\documentclass{ctexart}`. 并且将 `xelatex`作为你的编译器。
 为了能够在latexmk中使用`xelatex`, 只需添加  
 
     $pdflatex="xelatex %O %S";
 
-到你的`$HOME/latexmkrc`.
+到`$HOME/latexmkrc`.
 ## 2. 编辑环境美化
 ### 2.1 Zathura
-Zathura 是一个轻量级的 pdf 查看器，他有着和vim差不多的快捷键. 
-最爽的一点在于他是高度可定制化的. 你可以通过
+Zathura 是一个轻量级的 pdf 查看器，他有着和vim差不多的快捷键. 你可以通过
     
     vim ~/.config/zathura/zathurarc
 
@@ -56,13 +54,13 @@ Zathura 是一个轻量级的 pdf 查看器，他有着和vim差不多的快捷�
 这样当我使用`ctrl R` 进入夜间模式时，其背景颜色就和我的vim一样了，看起来就很舒服。
 
 ### 2.2 vim美化
-这个我并没有花太多时间，网上也有很多教程。 我只修改了 colorscheme 以及安装了一个vim-airline. 这些全部是通过
-vim-plug配置的（一个插件管理器）, 安装和更新都十分方便. 更多细节可以查看 `.vimrc` .
+这个没有研究太多，我用了`palenight`和`vim-airline`.
+以及vim-plug（一个插件管理器）, 安装和更新插件都十分方便. 更多细节可以查看 `.vimrc` .
 ## 3 快速做图 （可选）
-`inkscape` 是一个极为强大的开源作图软件，支持所有平台。最关键的是它支持输出
+`inkscape` 是一个极为强大的开源作图软件，支持所有平台。它支持输出
 `.pdf_tex`，也就是说你可以在图里自由的加入公式。配合Gilles开发的快捷键工具，你可以很快的输出高质量图片。
 <img src="./figures/inkscape.png" alt="">
-但是这个软件需要一些学习成本，而且可能并不是每个人都需要作图，所以他的下载是可选的。
+这个软件需要一些学习成本，而且可能并不是每个人都需要作图，所以下载与否需要考虑一下。
 ## 4. 致谢
 非常感谢 <a href="https://github.com/gillescastel">Gilles Castel</a>的博客. 
 我拜读了他的几篇博客感觉醍醐灌顶。这真的帮我省了很多时间。
--- install.sh
+++ install.sh
@@ -1,27 +1,24 @@
-echo "Happytexing start"
 echo "[1/7] Downloading vim-plug..."
 
 if [ -e $HOME/.vim/autoload/plug.vim ]
 then
-		echo "Nice! You already got vim-plug."
+		echo "You already got vim-plug."
 else
-    	echo "Progress start, it may take 60 seconds..."
+   		curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
-# curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-#     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo "[2/7] Installing zathura..."
 if [ 3 -le $(apt list 2>/dev/null | grep zathura | wc -l ) ]
 then
-        echo "Nice! You already got zathura."
+        echo "You already got zathura."
 else
-        echo "Progress start, it may take several seconds..."
- 		# sudo apt install zathura
+ 		sudo apt install zathura
 fi
 
 echo "[3/7] Configuring zathura..."
-#mv ./zathura.rc $HOME/.config
+mv ./zathurarc $HOME/.config
 
-echo "[4/7] Adding 5 plugins to your vim-plug install list, it may take several seconds...."
+echo "[4/7] Adding 5 plugins to your vim-plug"
 echo "Do you want your .vimrc to be replaced by the one in the repo?" 
 echo "Input y or n"
 read name
@@ -29,8 +26,7 @@ case $name in
 		Y | y)
 			if [ -e $HOME/.vimrc ] 
 			then
-				#cat << EOF >> $HOME/.vimrc
-				cat << EOF >> ./tmp.txt
+				cat << EOF >> $HOME/.vimrc
 call plug#begin('~/.vim/plugged')
 Plug 'lervag/vimtex'
 let g:tex_flavor='latex'
@@ -54,13 +50,11 @@ call plug#end()
 EOF
 			else 
 				echo "oh it seems like you didn't got .vimrc?"
-				echo "Let me help you with that!"
-				#cp ./.vimrc $HOME
+				cp ./.vimrc $HOME
 			fi;;
 
 		n | N)
-			echo "Copying .vimrc to $HOME...";;
-			#cp ./.vimrc $HOME	
+			echo ;;
 		*)
 			echo "error choice";;
 esac
@@ -71,14 +65,14 @@ source 2>/dev/null ~/.vimrc
 echo "[5/7] Installing Inkscape..."
 echo "Inkscape is a powerful svg drawing tool, it can also draw with latex"
 echo "With shortcut manager we can draw good figures very quick"
-echo "This part is optional, are you sure want download this?"
+echo "This part is optional, type 'y,n' to decide install or not"
 read dd
 case $dd in
          Y | y)
 			cd $HOME/Documents
-			#sudo apt install inkscape
-			#pip3 install inkscape-figures
-			#git clone https://github.com/gillescastel/inkscape-shortcut-manager.git
+			sudo apt install inkscape
+			pip3 install inkscape-figures
+			git clone https://github.com/gillescastel/inkscape-shortcut-manager.git
 			;;
 		 n | N)
 			echo "That's ok, let's move on";;
@@ -89,12 +83,10 @@ esac
 echo "[6/7] Moving tex.snippet to the directory"
 if [ -e $HOME/.vim/UltiSnips ]
 then
-		echo "moving tex.snippets to the right place..."
-		#cp ./tex.snippets $HOME/.vim/UltiSnips
+		cp ./tex.snippets $HOME/.vim/UltiSnips
 else 
-		echo "moving tex.snippets to the right place..."
-		#mkdir -p $HOME/.vim/UltiSnips
-		#cp ./tex.snippets $HOME/.vim/UltiSnips
+		mkdir -p $HOME/.vim/UltiSnips
+		cp ./tex.snippets $HOME/.vim/UltiSnips
 fi
  
 
@@ -102,7 +94,7 @@ echo "[7/7] Install them manually"
 echo "Hey, it's almost done now. But there is one more thing to do "
 echo "manually. Open vim and type <:PlugInstall PlugName> to install"
 echo "For example: <:PlugInstall vimtex>."
-echo "Then you can have a good texing"
+echo "Then you can have a good time texing"
 
 
 
